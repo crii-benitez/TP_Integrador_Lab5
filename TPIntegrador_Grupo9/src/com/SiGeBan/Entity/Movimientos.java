8580ecee64ae792aeb6a4cc74afb4ed39deed507
Gustavo <gustavopavichevich@hotmail.com>
2021-06-20 18:39:33 -0300

    Actualizo el POM con dependencias

---
+++ TPIntegrador_Grupo9/src/com/SiGeBan/Entity/Movimientos.java
@@ -0,0 +1,58 @@
+package com.SiGeBan.Entity;
+
+import java.util.Date;
+
+public class Movimientos {
+	private Cuentas numeroDecuentaOrigen;
+	private Cuentas numeroDecuentaDestino;
+	private Date fechaDeMovimiento;
+	private String detalle;
+	private double importe;
+	
+	public Movimientos() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	public Cuentas getNumeroDecuentaOrigen() {
+		return numeroDecuentaOrigen;
+	}
+
+	public void setNumeroDecuentaOrigen(Cuentas numeroDecuentaOrigen) {
+		this.numeroDecuentaOrigen = numeroDecuentaOrigen;
+	}
+
+	public Cuentas getNumeroDecuentaDestino() {
+		return numeroDecuentaDestino;
+	}
+
+	public void setNumeroDecuentaDestino(Cuentas numeroDecuentaDestino) {
+		this.numeroDecuentaDestino = numeroDecuentaDestino;
+	}
+
+	public Date getFechaDeMovimiento() {
+		return fechaDeMovimiento;
+	}
+
+	public void setFechaDeMovimiento(Date fechaDeMovimiento) {
+		this.fechaDeMovimiento = fechaDeMovimiento;
+	}
+
+	public String getDetalle() {
+		return detalle;
+	}
+
+	public void setDetalle(String detalle) {
+		this.detalle = detalle;
+	}
+
+	public double getImporte() {
+		return importe;
+	}
+
+	public void setImporte(double importe) {
+		this.importe = importe;
+	}
+
+
+}
---
+++ TPIntegrador_Grupo9/src/com/SiGeBan/Entity/TipoDeCuenta.java
@@ -0,0 +1,19 @@
+package com.SiGeBan.Entity;
+
+public class TipoDeCuenta {
+	
+	private String nombreDeTipoDeCuenta;
+	
+	public String getNombreDeTipoDeCuenta() {
+		return nombreDeTipoDeCuenta;
+	}
+
+	public void setNombreDeTipoDeCuenta(String nombreDeTipoDeCuenta) {
+		this.nombreDeTipoDeCuenta = nombreDeTipoDeCuenta;
+	}
+
+	public TipoDeCuenta() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
