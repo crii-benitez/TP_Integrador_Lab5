fc10793a75dcc43c61446306279580d0ae4c628c
Cristian Benitez <crii-benitez>
2021-06-21 16:47:11 -0300

    se agregan las entidades genero y localidades
    Ambas con id tipo entity, su descripcion getters, setters y toString

---
+++ TPIntegrador_Grupo9/src/com/SiGeBan/Entity/Generos.java
@@ -0,0 +1,43 @@
+package com.SiGeBan.Entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Gender")
+public class Generos implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="gender")
+	private String genero;
+	
+	public Generos()
+	{}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	@Override
+	public String toString() {
+		return "Genero [id=" + id + ", genero=" + genero + "]";
+	}
+	
+	
+}
---
+++ TPIntegrador_Grupo9/src/com/SiGeBan/Entity/Localidades.java
@@ -0,0 +1,44 @@
+package com.SiGeBan.Entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Cities")
+public class Localidades implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="city")
+	private String ciudad;
+	
+	public Localidades()
+	{}
+
+	public String getCiudad() {
+		return ciudad;
+	}
+
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+
+	@Override
+	public String toString() {
+		return "Localidades [id=" + id + ", ciudad=" + ciudad + "]";
+	}
+	
+	
+	
+}
