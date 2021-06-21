723e192610d020b8626d7c122725db1c7375b70c
German Medina <gmedinasistemas@gmail.com>
2021-06-21 01:29:58 -0300

    Termino La clase Perfiles, y la clase Usuario.

--- TPIntegrador_Grupo9/src/com/SiGeBan/Entity/Paises.java
+++ TPIntegrador_Grupo9/src/com/SiGeBan/Entity/Paises.java
@@ -2,7 +2,32 @@ package com.SiGeBan.Entity;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Table(name = "Countries")
 public class Paises implements Serializable {
+	/**
+	 * //Implementar serializable
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@Column(name="Country")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private String Pais;
+	@Override
+	public String toString() {
+		return "Paises [Pais=" + Pais + "]";
+	}
+	public String getPais() {
+		return Pais;
+	}
+	public void setPais(String pais) {
+		Pais = pais;
+	}
 
 }
--- TPIntegrador_Grupo9/src/com/SiGeBan/Entity/Usuario.java
+++ TPIntegrador_Grupo9/src/com/SiGeBan/Entity/Usuario.java
@@ -2,10 +2,61 @@ package com.SiGeBan.Entity;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Table(name = "Users")
 public class Usuario implements Serializable {
 
+	/**
+	 * //Implementar serializable
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String user;
+	private String pass;
+	private Perfiles perfil;
+	@Override
+	public String toString() {
+		if(perfil == null)
+			return "Usuario [id=" + id + ", user=" + user + ", pass=" + pass +  "]";
+		else
+			return "Usuario [id=" + id + ", user=" + user + ", pass=" + pass + ", perfil=" + perfil + "]";
+	}
+	public Perfiles getPerfil() {
+		return perfil;
+	}
+	public void setPerfil(Perfiles perfil) {
+		this.perfil = perfil;
+	}
 	public Usuario() {
 		// TODO Auto-generated constructor stub
 	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
 
 }
